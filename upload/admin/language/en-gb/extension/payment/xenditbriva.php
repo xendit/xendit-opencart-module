cf195ae2da104c4b3878d620fece045ebdf520fe
Aulia Hakiem <hakiemaul@pop-os.localdomain>
2019-08-06 21:12:21 +0700

    [TPI-507] add image to extension manager

--- upload/admin/language/en-gb/extension/payment/xenditbriva.php
+++ upload/admin/language/en-gb/extension/payment/xenditbriva.php
@@ -8,6 +8,7 @@ $_['text_test_mode']    = 'Test';
 $_['text_live_mode']    = 'Live';
 $_['text_success']      = 'Success: You have changed Xendit payment module settings!';
 $_['text_edit']         = 'Xendit BRI VA Configuration';
+$_['text_xenditbriva']  = '<a target="_BLANK" href="https://xendit.co/"><img src="view/image/payment/xendit.png" alt="Xendit" title="Xendit" style="border: 1px solid #EEEEEE; max-width:100px;" /></a>';
 
 // Entries
 $_['entry_status']      = 'Status';
--- upload/admin/language/en-gb/extension/payment/xenditpermatava.php
+++ upload/admin/language/en-gb/extension/payment/xenditpermatava.php
@@ -4,10 +4,11 @@
 $_['heading_title']     = 'Xendit - Permata VA';
 
 // Texts
-$_['text_test_mode']    = 'Test';
-$_['text_live_mode']    = 'Live';
-$_['text_success']      = 'Success: You have changed Xendit payment module settings!';
-$_['text_edit']         = 'Xendit Permata VA Configuration';
+$_['text_test_mode']        = 'Test';
+$_['text_live_mode']        = 'Live';
+$_['text_success']          = 'Success: You have changed Xendit payment module settings!';
+$_['text_edit']             = 'Xendit Permata VA Configuration';
+$_['text_xenditpermatava']  = '<a target="_BLANK" href="https://xendit.co/"><img src="view/image/payment/xendit.png" alt="Xendit" title="Xendit" style="border: 1px solid #EEEEEE; max-width:100px;" /></a>';
 
 // Entries
 $_['entry_status']      = 'Status';
