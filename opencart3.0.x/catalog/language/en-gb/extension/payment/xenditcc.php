c4f5f571ed7b99273eb6f8c155a5a13cffd14967
Aulia Hakiem <noersedya@gmail.com>
2020-01-13 15:05:06 +0700

    [TPI-1044] remove duplicated files in oc 3.0

--- opencart3.0.x/catalog/language/en-gb/extension/payment/xenditcc.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Text
-$_['text_title'] = 'Credit Card';
-$_['text_instructions'] = 'Please fill these fields below to pay this order through <strong>Xendit</strong>';
-$_['text_test_instructions'] = 'TEST MODE - This transaction is for testing. Real payment will not be detected';
