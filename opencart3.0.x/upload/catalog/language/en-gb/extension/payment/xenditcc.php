d5b87ace4b68976997ea11fc68c0854c0078a0fd
kevin.alfianto <kevin.jangtjik@xendit.co>
2021-09-20 23:26:10 +0700

    add cc invoice payment method

--- opencart3.0.x/upload/catalog/language/en-gb/extension/payment/xenditcc.php
+++ opencart3.0.x/upload/catalog/language/en-gb/extension/payment/xenditcc.php
@@ -3,3 +3,6 @@
 $_['text_title'] = 'Credit Card';
 $_['text_instructions'] = 'Please fill these fields below to pay this order through <strong>Xendit</strong>';
 $_['text_test_instructions'] = 'TEST MODE - This transaction is for testing. Real payment will not be detected';
+$_['text_failure'] = 'Payment failed, please try again.';
+
+$_['heading_title'] = 'Xendit Payment';
